ddadd1b2d71db3b16d2ff72e08396c6b24b05136
julia-allyce <julia@gertrude.tech>
2026-03-14 12:41:23 -0600

    update base path for stories

--- .ladle/config.mjs
+++ .ladle/config.mjs
@@ -3,6 +3,7 @@ export default {
   stories: 'story-src/**/*.stories.{jsx,tsx}',
   outDir: 'stories',
   port: 61000,
+  base: 'https://julia-allyce.github.io/react-force-graph/stories/',
   addons: {
     control: { enabled: true },
     source: { enabled: true, defaultState: 'hide' },
